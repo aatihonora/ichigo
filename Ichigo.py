245c94071196e7eff3da6497df1ad276eaddf85c
azogthesmoker <behramtalpur2000@gmail.com>
2020-03-03 04:02:30 +0500

    Initial commit

---
+++ Ichigo.py
@@ -0,0 +1,24 @@
+import discord
+
+TOKEN = 'NjUyMDY3MjUzMDgwMDMxMjMz.XjQjAw.3oMQ24V1m3Gvdww__9IamQT9zIA'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)

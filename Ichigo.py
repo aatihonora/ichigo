e5e1b219bb177d2d8cbc6ccf16d52cc301691707
azogthesmoker <58216695+azogthesmoker@users.noreply.github.com>
2020-03-03 18:26:51 +0500

    a

--- Ichigo.py
+++ Ichigo.py
@@ -10,7 +10,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('hi'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
